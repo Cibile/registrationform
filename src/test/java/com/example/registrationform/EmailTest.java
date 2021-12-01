31292697b5b8f86964601f5e4ebc244106cbe512
cibil <cibilecyriac@gmail.com>
2021-12-01 11:56:43 +0000

    Test class for Email, created one unit test to check if constructor works as intended.

---
+++ src/test/java/com/example/registrationform/EmailTest.java
@@ -0,0 +1,22 @@
+package com.example.registrationform;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Test Email")
+class EmailTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void checkConstructorValid() {
+        String testEmail = "hi@google.com";
+        Email email = new Email(testEmail);
+        assertEquals(testEmail, email.getEmail());
+    }
+}
