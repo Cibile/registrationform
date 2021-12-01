fe40b821153f1ac1bf52fd7d3e6e8a963e6c5959
cibil <cibilecyriac@gmail.com>
2021-12-01 11:53:21 +0000

    Created class Email to ensure email is well-formed after unit tests.

---
+++ src/main/java/com/example/registrationform/Email.java
@@ -0,0 +1,13 @@
+package com.example.registrationform;
+
+public class Email {
+    private final String email;
+
+    public Email(final String email){
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
