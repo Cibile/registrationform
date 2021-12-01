3740779902ad438d9f6b7b09804cf8631240f6a3
cibil <cibilecyriac@gmail.com>
2021-12-01 11:57:38 +0000

    committing files

---
+++ src/test/java/com/example/registrationform/HelloApplicationTest.java
@@ -0,0 +1,23 @@
+package com.example.registrationform;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void main() {
+    }
+}
