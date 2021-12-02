d64380fc7b3958cda6a4190347ad54f7feafaf56
cibil <cibilecyriac@gmail.com>
2021-12-02 12:00:42 +0000

    Create class Password to check user input for password.

---
+++ src/main/java/com/example/registrationform/Password.java
@@ -0,0 +1,13 @@
+package com.example.registrationform;
+
+public class Password {
+    private final String password;
+
+    public Password(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+}
