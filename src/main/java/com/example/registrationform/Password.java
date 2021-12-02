0045306f35cefd4403df3f23409f665ab6a26f78
cibil <cibilecyriac@gmail.com>
2021-12-02 12:50:44 +0000

    Edited Password class to pass unit tests for minimum length.

--- src/main/java/com/example/registrationform/Password.java
+++ src/main/java/com/example/registrationform/Password.java
@@ -2,9 +2,14 @@ package com.example.registrationform;
 
 public class Password {
     private final String password;
+    private final int MIN_PASSWORD_LENGTH = 7;
 
     public Password(String password){
-        this.password = password;
+        if(password.length() >= MIN_PASSWORD_LENGTH){
+            this.password = password;
+        }else {
+            this.password = "Invalid password";
+        }
     }
 
     public String getPassword(){
