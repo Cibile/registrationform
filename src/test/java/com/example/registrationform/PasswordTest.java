0fb952a52b76e2571095a520f49b08cba5f16f1d
cibil <cibilecyriac@gmail.com>
2021-12-02 12:48:27 +0000

    Unit tests to make sure Password only constructed if length greater than or equal to 7.

---
+++ src/test/java/com/example/registrationform/PasswordTest.java
@@ -0,0 +1,29 @@
+package com.example.registrationform;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @DisplayName("Password length less than 7")
+    void checkPasswordWithoutMinLength() {
+        String testPassword = "pass";
+        Password password = new Password(testPassword);
+        assertEquals("Invalid password", password.getPassword());
+    }
+
+    @Test
+    void checkPasswordWithMinLength(){
+        String testPassword = "password123";
+        Password password = new Password(testPassword);
+        assertEquals(testPassword, password.getPassword());
+    }
+}
