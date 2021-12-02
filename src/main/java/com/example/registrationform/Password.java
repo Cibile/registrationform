72a1853de3085ebfd59ed698765a7db5e303b946
cibil <cibilecyriac@gmail.com>
2021-12-02 13:30:45 +0000

    Password constructor makes sure password contains at least one letter.

--- src/main/java/com/example/registrationform/Password.java
+++ src/main/java/com/example/registrationform/Password.java
@@ -1,11 +1,14 @@
 package com.example.registrationform;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Password {
     private final String password;
     private final int MIN_PASSWORD_LENGTH = 7;
 
     public Password(String password){
-        if(password.length() >= MIN_PASSWORD_LENGTH){
+        if(password.length() >= MIN_PASSWORD_LENGTH && checkAtLeastOneLetter(password)){
             this.password = password;
         }else {
             this.password = "Invalid password";
@@ -14,5 +17,12 @@ public class Password {
 
     public String getPassword(){
         return password;
+    }
+
+    public boolean checkAtLeastOneLetter(String password){
+        String regex = ".*[a-zA-Z].*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
     }
 }
